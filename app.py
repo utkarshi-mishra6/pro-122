da8ef7b2eb58eb52214dd1c6e41d02d3c72291e3
utkarshi-mishra6 <80529529+utkarshi-mishra6@users.noreply.github.com>
2022-02-28 12:52:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import cv2
+from flask import Flask, jsonify, request
+from classifier import  get_alphabet
+import numpy as np
+
+app = Flask(__name__)
+
+# defining the portal
+@app.route("/predict-alphabet", methods=["POST"])
+
+# writing function for predicting
+def predict_data():
+  image = cv2.imdecode(np.fromstring(request.files.get("alphabet").read(), np.uint8), cv2.IMREAD_UNCHANGED)
+  image = request.files.get("alphabet")
+  alphabet = get_alphabet(image)
+  return jsonify({
+    "alphabet_predicted": alphabet
+  }), 200
+
+# running the function
+if __name__ == "__main__":
+  app.run(debug=True)
